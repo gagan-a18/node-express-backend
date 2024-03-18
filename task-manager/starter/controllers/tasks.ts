743c77a2f95b1f9d65ac2e4358f5caeefef2143d
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-03-18 14:38:06 +0530

    connection to mongo db...

--- task-manager/starter/controllers/tasks.ts
+++ task-manager/starter/controllers/tasks.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
+import Task from "../models/modeltasks";
+
 
 
 const getAllTasks = (req: Request, res: Response) => {
     res.send('All items');
 }
 
-const createTasks = (req: Request, res: Response) => {
-    res.send('Create Task');
+const createTasks = async (req: Request, res: Response) => {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
 }
 
 const getSingleTask = (req: Request, res: Response) => {
---
+++ task-manager/starter/models/modeltasks.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+
+const TaskSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean
+})
+
+export default mongoose.model('Task', TaskSchema);
+
