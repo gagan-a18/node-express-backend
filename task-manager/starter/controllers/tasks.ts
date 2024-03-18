daced0d1a7f8bd2fbcb7bd2a09faf56641684eaa
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-03-18 17:04:57 +0530

    added validations and handles promise

--- task-manager/starter/controllers/tasks.ts
+++ task-manager/starter/controllers/tasks.ts
@@ -3,16 +3,21 @@ import Task from "../models/modeltasks";
 
 
 
-const getAllTasks = (req: Request, res: Response) => {
+const getAllTasks = (req: Request, res: Response): void => {
     res.send('All items');
 }
 
-const createTasks = async (req: Request, res: Response) => {
-    const task = await Task.create(req.body);
-    res.status(200).json({ task });
+const createTasks = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(200).json({ task });
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
 }
 
-const getSingleTask = (req: Request, res: Response) => {
+const getSingleTask = (req: Request, res: Response): void => {
     res.send('Single Task');
 }
 
