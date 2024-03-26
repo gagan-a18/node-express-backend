51974365423250f566e4228af8e837af8b82c950
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-03-26 20:03:19 +0530

    SetUp specific error responses and wrapping up JWT

---
+++ JWT/errors/badrequest.ts
@@ -0,0 +1,10 @@
+class ErrorRequest extends Error {
+    statusCode: number
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export default ErrorRequest;
--- JWT/routes/middleware/tasks.ts
+++ JWT/routes/middleware/tasks.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express"
 import jwt from "jsonwebtoken";
+import ErrorRequest from "../../errors/badrequest";
+
+interface JwtPayload {
+    username?: string;
+}
 
 const login = (req: Request, res: Response) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        throw Error('BAD Request');
+        throw new ErrorRequest('BAD Request', 400);
     }
     const id = new Date().getDate();
 
@@ -12,10 +17,24 @@ const login = (req: Request, res: Response) => {
     res.status(200).send({ msg: 'User Created', token });
 }
 
-const dashboard = async (req: Request, res: Response) => {
-    const number = Math.floor(Math.random() * 100);
-    res.status(200).json({ 'msg': 'hello', secret: `Your number is ${number}` })
+const dashboard = (req: Request, res: Response) => {
 
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new ErrorRequest('No token provided', 401);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
+        const number = Math.floor(Math.random() * 100);
+        res.status(200).json({ 'msg': `Hello ${decoded.username}`, secret: `Your number is ${number}` })
+    }
+    catch (error) {
+        throw new ErrorRequest('Not authorized to access this route', 401)
+    }
 }
 
 export { login, dashboard };
