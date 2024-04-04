d29671692a62da3eca7a661c42e4e54d4fc8c4bf
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-04-04 18:48:21 +0530

    added mongoose methods and prehooks for efficiency

--- Jobs/middleware/authentication.ts
+++ Jobs/middleware/authentication.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import userModel from "../models/userModel";
-import ErrorRequest from "../errors/errorRequest";
+
 
 const register = async (req: Request, res: Response) => {
+
     try {
         const user = await userModel.create({ ...req.body });
-        res.status(200).send({ user });
+        const token = user.createJWT();
+        res.status(200).send({ user: { name: user.name }, token });
     }
     catch (error) {
         res.status(400).send(error);
