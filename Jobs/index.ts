f314807cea9ca98041b1869de578ab149a7ad733
gagan-a18 <94117543+gagan-a18@users.noreply.github.com>
2024-04-08 19:49:04 +0530

    setup swagger

--- Jobs/index.ts
+++ Jobs/index.ts
@@ -5,7 +5,13 @@ import connectDb from "./connection/connectDb";
 import { authRouter, jobRouter } from "./routes/routes";
 import errorHandler from "./errors/errorHandler";
 import tokenAuth from "./middleware/tokenAuth";
+import helmet from "helmet";
+import cors from "cors";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 dotenv.config();
 
@@ -14,11 +20,16 @@ const app: express.Application = express();
 const port: Number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/domain/api/v1/auth', authRouter);
 app.use('/domain/api/v1/jobs', tokenAuth, jobRouter);
 
 app.use(errorHandler);
+
 
 const startServer = async () => {
     await connectDb(`${process.env.MONGO_DB_URI}`)
